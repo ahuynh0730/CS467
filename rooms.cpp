db315994f73faec1377b6319c7e6c93871b1097a
Anthony Huynh <ahuynh0730@gmail.com>
2020-04-25 12:11:13 -0500

    Created room structure

---
+++ rooms.cpp
@@ -0,0 +1,3 @@
+#include "rooms.hpp"
+#include <iostream>
+
---
+++ rooms.hpp
@@ -0,0 +1,17 @@
+#ifndef ROOMS_hpp
+#define ROOMS_hpp
+
+
+class Room{
+private:
+	int roomNumber = -1;
+	Room* North;
+	Room* West;
+	Room* South;
+	Room* East;
+
+
+
+};
+
+#endif
