05de10870c1486e45c815fd608bd0bf816593677
nseabourn <nseabourn001@gmail.com>
2020-05-02 10:22:11 -0700

    most of quiz added, need to complete answer function

--- Interactables/Suspect.cpp
+++ Interactables/Suspect.cpp
@@ -5,6 +5,14 @@ Suspect::Suspect(): Interactable{}, isMurderer{false}{}
 Suspect::Suspect(bool m, std::string name_, std::string desc, Room* r, Game* g):
 	Interactable{std::string name_, std::string desc, Room* r, Game* g, false}, isMurderer{m}{}
 
+bool Suspect::getIsMurderer(){
+	return isMurderer;
+}
+
+void Suspect::setIsMurderer(bool m){
+	isMurderer = m;
+}
+
 std::string Suspect::accuse(){
 	game->setGameOver(true);
 	if(isMurderer){
--- Interactables/Suspect.hpp
+++ Interactables/Suspect.hpp
@@ -6,10 +6,11 @@
 class Suspect: public Interactable{
 private:
 	bool isMurderer;
-	int userOptions;
 public:
 	Suspect();
 	Suspect(bool m, std::string name_, std::string desc, Room* r, Game* g);
+	bool getIsMurderer();
+	void setIsMurderer(bool m);
 	std::vector<std::string> listUserOptions();
 	std::string callFunction(int n);
 	std::string accuse();
